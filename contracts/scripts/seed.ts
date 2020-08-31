b2dab98fccbdc04c63df1bce6f694025b76f41ab
Wighawag <wighawag@gmail.com>
2020-08-31 13:52:47 +0100

    use svite

---
+++ contracts/scripts/seed.ts
@@ -0,0 +1,35 @@
+import {getNamedAccounts, ethers, deployments} from '@nomiclabs/buidler';
+import {{{=_.pascalCase(it.contractName)}}} from 'common-lib';
+
+const names = [
+  "Hetrorlig Oakenbrow",
+  "Dermerlug Brewcloak",
+  "Nalol Blackbraids",
+  "Webir Goldenback",
+  "Dholdrec Chaosgrip",
+  "Siggog Strongjaw",
+  "Krostol Snowtank",
+  "Rumit Dragonarmour",
+  "Nemnad Thunderbrow",
+  "Gagham Grimbelly",
+];
+
+// TODO move to util
+const waitFor = <T>(p: Promise<{wait: () => Promise<T>}>) => p.then((tx) => tx.wait());
+
+async function main() {
+  for (let i = 0; i < 4; i++) {
+    const {others} = await getNamedAccounts();
+    for (let i = 0; i < 4; i++) {
+      const {{=_.camelCase(it.contractName)}}Contract = await ethers.getContract("{{=_.pascalCase(it.contractName)}}", others[i]);
+      await waitFor({{=_.camelCase(it.contractName)}}Contract.setName(names[i]));
+    }
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ web/src/pages/home.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import CommonLib from 'common-lib';
+
+  import {logs} from 'named-logs';
+  const console = logs('app:home');
+  console.log('CommongLib', CommonLib);
+</script>
+
+<h1> Hello world </h1>
